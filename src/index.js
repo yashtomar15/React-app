ac6d4fd580f50ed5fc8f71af14aff5282fe5b85c
Yash Tomar <99667382+yashtomar15@users.noreply.github.com>
2022-05-02 19:02:02 -1200

    Delete src directory

--- src/index.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-import {createRoot} from 'react-dom/client'
-import {App} from './App.js'
- 
-var container=document.getElementById("root");
-var root=createRoot(container);
-root.render(<App />);
